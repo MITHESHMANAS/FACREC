1d67869b556e2d95ae9fd648da1eaa825e4956f7
MITHESHMANAS <nmithesh06@gmail.com>
2026-06-13 20:09:56 +0530

    created register and view students file

---
+++ register_student.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+conn = sqlite3.connect("attendance.db")
+cursor = conn.cursor()
+
+name = input("Enter Name : ")
+roll_no = input("Enter Roll Number : ")
+branch = input("Enter Branch : ")
+semester = int(input("Enter Semester : "))
+email = input("Enter Email : ")
+
+cursor.execute("""
+INSERT INTO students
+(name, roll_no, branch, semester, email)
+VALUES (?, ?, ?, ?, ?)
+""",
+(name, roll_no, branch, semester, email))
+
+conn.commit()
+conn.close()
+
+print("Student Registered Successfully")
