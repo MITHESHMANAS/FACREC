89337759e6d74e51ef394076e54c13ec2c73e926
MITHESHMANAS <nmithesh06@gmail.com>
2026-07-04 22:38:59 +0530

    Add subject service for CRUD operations

--- client/src/services/subjectService.js
+++ client/src/services/subjectService.js
@@ -1,17 +1,21 @@
 import api from "./api";
 
 export const getSubjects = async () => {
-
-    const response = await api.get("/subjects");
-
-    return response.data;
-
+    const { data } = await api.get("/subjects");
+    return data;
 };
 
 export const createSubject = async (subject) => {
+    const { data } = await api.post("/subjects", subject);
+    return data;
+};
 
-    const response = await api.post("/subjects", subject);
+export const updateSubject = async (id, subject) => {
+    const { data } = await api.put(`/subjects/${id}`, subject);
+    return data;
+};
 
-    return response.data;
-
+export const deleteSubject = async (id) => {
+    const { data } = await api.delete(`/subjects/${id}`);
+    return data;
 };
